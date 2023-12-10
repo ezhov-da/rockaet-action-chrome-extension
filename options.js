6ccb3ebe790ae709c9e35e755f05974c62e08fcb
ezhov-da <ezhdenis@yandex.ru>
2023-12-10 17:09:58 +0300

    feat: added save action ID to storage

--- options.js
+++ options.js
@@ -1,23 +1,79 @@
+function addAction() {
+    const raActionId = document.getElementById("raActionId").value;
+    const usageInputParameter = document.getElementById("usageInputParameter").value;
+
+
+    getOptions().then((object) => {
+        if (object.actions === undefined) {
+            object.actions = []
+        }
+
+        object.actions.push({
+            id: raActionId,
+            useInput: Boolean(usageInputParameter)
+        })
+
+        saveAllOptions(object).then(() => {
+            fillList(object)
+        })
+    })
+}
+
+function fillListFromSavedOptions(object) {
+    getOptions().then((object) => {
+        fillList(object)
+    })
+}
+
+function fillList(object) {
+    const actionsElement = document.getElementById("actions");
+    while (actionsElement.firstChild) {
+        actionsElement.removeChild(actionsElement.firstChild);
+    }
+
+    console.log(object.actions)
+
+    object.actions.forEach((value) => {
+        let newElement = document.createElement("p",)
+        newElement.innerText = value.id
+
+        actionsElement.appendChild(newElement);
+    })
+}
+
+
 function saveOptions() {
     const url = document.getElementById("raUrl").value;
     const key = document.getElementById("raApiKey").value;
 
-    const options = {apiUrl: url, apiKey: key}
-    chrome.storage.local.set(options).then(() => {
-        console.log(options);
-    });
+    getOptions().then((object) => {
+        object.apiUrl = url
+        object.apiKey = url
+
+        saveAllOptions(object)
+    })
 }
 
 function loadOptions() {
     getOptions().then((result) => {
-        document.getElementById("raUrl").value = result.apiUrl;
-        document.getElementById("raApiKey").value = result.apiKey;
+        if (result.apiUrl) {
+            document.getElementById("raUrl").value = result.apiUrl;
+        }
+        if (result.apiKey) {
+            document.getElementById("raApiKey").value = result.apiKey;
+        }
     })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     loadOptions()
+    fillListFromSavedOptions()
 });
+
 document.getElementById('saveOptions').addEventListener('click', () => {
     saveOptions()
+});
+
+document.getElementById('addAction').addEventListener('click', () => {
+    addAction()
 });
--- storage.js
+++ storage.js
@@ -1,3 +1,7 @@
 async function getOptions() {
-    return chrome.storage.local.get(["apiUrl", "apiKey"])
+    return chrome.storage.local.get()
+}
+
+async function saveAllOptions(object) {
+    return chrome.storage.local.set(object)
 }
